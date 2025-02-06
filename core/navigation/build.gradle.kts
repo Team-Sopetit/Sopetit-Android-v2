92f6639c859f36da7795bd06d9a784d024ec1460
emjayMJkim <minjeong030420@gmail.com>
2025-02-07 01:06:44 +0900

    #1 [add] navigation 모듈 의존성 추가

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -9,4 +9,6 @@ android {
 
 dependencies {
     implementation(projects.core)
+
+    implementation(projects.feature.onboarding)
 }
--- core/navigation/src/main/java/com/sopetit/navigation/NavRoutes.kt
+++ core/navigation/src/main/java/com/sopetit/navigation/NavRoutes.kt
@@ -1,4 +1,8 @@
 package com.sopetit.navigation
 
 sealed class NavRoutes(val route: String) {
+
+    // OnBoarding Graph
+    data object OnBoardingGraph: NavRoutes("onboarding_graph")
+    data object OnBoardingScreen: NavRoutes("onboarding")
 }
