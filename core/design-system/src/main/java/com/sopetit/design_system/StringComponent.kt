81113d656442ae32cdba33c0646e89a029ba2993
emjayMJkim <minjeong030420@gmail.com>
2025-02-09 00:54:01 +0900

    #3 [feat] 스토리텔링 화면3 구현

--- core/design-system/src/main/java/com/sopetit/design_system/StringComponent.kt
+++ core/design-system/src/main/java/com/sopetit/design_system/StringComponent.kt
@@ -6,3 +6,4 @@ package com.sopetit.design_system
 // Onboarding
 const val StoryTellingContent1 = "오늘도 고된 하루였어.."
 const val StoryTellingContent2 = "어? 집 앞에 이건 뭐지?"
+const val StoryTellingContent3 = "한 번 열어볼까?"
---
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/storytelling/StoryTellingThirdScreen.kt
@@ -0,0 +1,83 @@
+package com.sopetit.onboarding.storytelling
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopetit.design_system.Gray50
+import com.sopetit.design_system.R
+import com.sopetit.design_system.StoryTellingContent3
+import com.sopetit.ui.common.StoryTellingTextContent
+
+@Composable
+fun StoryTellingThirdScreen() {
+    StoryTellingThirdContent()
+}
+
+@Composable
+fun StoryTellingThirdContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray50)
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_door),
+                    contentDescription = "story telling girl",
+                    modifier = Modifier
+                        .size(width = 211.dp, height = 390.dp)
+                        .padding(bottom = 40.dp)
+                        .align(Alignment.Center)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_box_closed),
+                    contentDescription = "story telling closed box",
+                    modifier = Modifier
+                        .size(width = 141.dp, height = 120.dp)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(63.dp))
+
+            Box(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(bottom = 107.dp)
+            ) {
+                StoryTellingTextContent(
+                    storyContent = StoryTellingContent3
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewStoryTellingThird() {
+    StoryTellingThirdContent()
+}
