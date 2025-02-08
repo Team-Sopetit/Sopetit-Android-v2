9cda7cd95e0a49621921f3fee4c3402041caad61
emjayMJkim <minjeong030420@gmail.com>
2025-02-09 00:10:32 +0900

    #3 [refactor] 스토리텔링 content 공통 ui 생성 및 적용

---
+++ core/ui/src/main/java/com/sopetit/ui/common/StoryTellingTextContent.kt
@@ -0,0 +1,60 @@
+package com.sopetit.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopetit.design_system.Gray0
+import com.sopetit.design_system.Gray700
+import com.sopetit.design_system.R
+import com.sopetit.design_system.SoftieTypo
+
+@Composable
+fun StoryTellingTextContent(
+    storyContent: String
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+            .clip(RoundedCornerShape(12))
+            .background(Gray0)
+            .wrapContentHeight()
+    ) {
+        Text(
+            text = storyContent,
+            style = SoftieTypo.bubble1,
+            color = Gray700,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(vertical = 23.dp)
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_btn_next),
+            contentDescription = "next btn",
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 23.dp)
+                .size(14.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewStoryTellingContent() {
+    StoryTellingTextContent(storyContent = "test")
+}
--- feature/onboarding/src/main/java/com/sopetit/onboarding/storytelling/StoryTellingFirstScreen.kt
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/storytelling/StoryTellingFirstScreen.kt
@@ -6,28 +6,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.sopetit.design_system.Gray0
 import com.sopetit.design_system.Gray50
-import com.sopetit.design_system.Gray700
 import com.sopetit.design_system.R
-import com.sopetit.design_system.SoftieTypo
 import com.sopetit.design_system.StoryTellingContent1
+import com.sopetit.ui.common.StoryTellingTextContent
 
 
 @Composable
@@ -68,39 +60,11 @@ fun StoryTellingContent() {
                     .wrapContentSize()
                     .padding(bottom = 107.dp)
             ) {
-                StoryTextContent()
+                StoryTellingTextContent(
+                    storyContent = StoryTellingContent1
+                )
             }
         }
-    }
-}
-
-@Composable
-fun StoryTextContent() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp)
-            .clip(RoundedCornerShape(12))
-            .background(Gray0)
-            .wrapContentHeight()
-    ) {
-        Text(
-            text = StoryTellingContent1,
-            style = SoftieTypo.bubble1,
-            color = Gray700,
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(vertical = 23.dp)
-        )
-
-        Icon(
-            painter = painterResource(id = R.drawable.ic_btn_next),
-            contentDescription = "next btn",
-            modifier = Modifier
-                .align(Alignment.CenterEnd)
-                .padding(end = 23.dp)
-                .size(14.dp)
-        )
     }
 }
 
