c0fbcf9d92b4db269aed631c5bd4454f13c31a0d
emjayMJkim <minjeong030420@gmail.com>
2025-02-19 02:05:45 +0900

    #5 [feat] 테마 선택 뷰 기본 화면 구현

--- feature/onboarding/src/main/java/com/sopetit/onboarding/themechoice/ThemeChoiceScreen.kt
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/themechoice/ThemeChoiceScreen.kt
@@ -1,16 +1,39 @@
 package com.sopetit.onboarding.themechoice
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.sopetit.design_system.Gray0
+import com.sopetit.design_system.Gray200
 import com.sopetit.design_system.Gray50
+import com.sopetit.design_system.Gray700
+import com.sopetit.design_system.R
+import com.sopetit.design_system.SoftieTypo
 import com.sopetit.design_system.ThemeChoiceBtn
+import com.sopetit.domain.entity.response.theme.ThemeListItemModel
 import com.sopetit.ui.common.item.BottomRectangleBtn
 import com.sopetit.ui.common.topbar.OnboardingTopBar
 
@@ -19,15 +42,18 @@ fun ThemeChoiceScreen(
     goBackToDollNamingPage: () -> Unit = {}
 ) {
     val viewModel: ThemeChoiceViewModel = hiltViewModel()
+    val uiState: ThemeChoicePageState by viewModel.uiState.collectAsStateWithLifecycle()
 
     ThemeChoiceContent(
-        onClickBackBtnAction = { goBackToDollNamingPage() }
+        onClickBackBtnAction = { goBackToDollNamingPage() },
+        themeList = uiState.themeList
     )
 }
 
 @Composable
 fun ThemeChoiceContent(
-    onClickBackBtnAction: () -> Unit = {}
+    onClickBackBtnAction: () -> Unit = {},
+    themeList: List<ThemeListItemModel> = emptyList()
 ) {
     Box(
         modifier = Modifier
@@ -50,13 +76,65 @@ fun ThemeChoiceContent(
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                //
+                ThemeChoiceList(
+                    themeList = themeList
+                )
             }
 
             BottomRectangleBtn(
                 btnTextContent = ThemeChoiceBtn
             )
         }
+    }
+}
+
+@Composable
+fun ThemeChoiceList(
+    themeList: List<ThemeListItemModel> = emptyList()
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(top = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        itemsIndexed(themeList, key = { _, item -> item.themeId }) { _, item ->
+            ThemeChoiceListItem(
+                themeItem = item
+            )
+        }
+    }
+}
+
+@Composable
+fun ThemeChoiceListItem(
+    themeItem: ThemeListItemModel = ThemeListItemModel()
+) {
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .clip(RoundedCornerShape(99.dp))
+            .border(1.dp, Gray200, RoundedCornerShape(99.dp))
+            .background(Gray0)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_theme1),
+            contentDescription = "theme icon",
+            modifier = Modifier
+                .padding(start = 20.dp)
+                .size(18.dp)
+                .align(Alignment.CenterVertically)
+        )
+
+        Text(
+            text = themeItem.title,
+            color = Gray700,
+            style = SoftieTypo.body1,
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .padding(start = 6.dp, end = 20.dp)
+        )
     }
 }
 
