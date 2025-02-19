f3bf3c72c20fe3dc5c21149be18910f5f6240093
emjayMJkim <minjeong030420@gmail.com>
2025-02-20 03:25:44 +0900

    #5 [feat] 루틴 목록 조회 서버통신

--- feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoicePageState.kt
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoicePageState.kt
@@ -1,9 +1,11 @@
 package com.sopetit.onboarding.routinechoice
 
 import com.sopetit.domain.entity.request.CreateMemberModel
+import com.sopetit.domain.entity.response.routine.DailyRoutineListModel
 import com.sopetit.ui.base.PageState
 
 data class RoutineChoicePageState (
     val memberModel: CreateMemberModel = CreateMemberModel(),
-    val selectedThemeIdList: List<Int> = emptyList()
+//    val selectedThemeIdList: List<Int> = emptyList()
+    val routineTotalList: List<DailyRoutineListModel> = emptyList()
 ): PageState
--- feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoiceViewModel.kt
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoiceViewModel.kt
@@ -1,15 +1,20 @@
 package com.sopetit.onboarding.routinechoice
 
+import androidx.lifecycle.viewModelScope
 import com.sopetit.domain.entity.request.CreateMemberModel
+import com.sopetit.domain.entity.request.routine.DailyRoutineListRequestModel
+import com.sopetit.domain.entity.response.routine.DailyRoutineListThemeTotalModel
+import com.sopetit.domain.usecase.routine.GetDailyRoutineUseCase
 import com.sopetit.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class RoutineChoiceViewModel @Inject constructor(
-
-): BaseViewModel<RoutineChoicePageState>(
+    private val getDailyRoutineUseCase: GetDailyRoutineUseCase
+) : BaseViewModel<RoutineChoicePageState>(
     RoutineChoicePageState()
 ) {
 
@@ -20,5 +25,22 @@ class RoutineChoiceViewModel @Inject constructor(
             )
         )
         Timber.d("[온보딩] (routineChoice) member -> ${uiState.value.memberModel}")
+        initGetDailyRoutineList()
+    }
+
+    private fun initGetDailyRoutineList() {
+        viewModelScope.launch {
+            getDailyRoutineUseCase(request = DailyRoutineListRequestModel(uiState.value.memberModel.selectedThemeIdList)).collect {
+                resultResponse(it, ::onSuccessGetDailyRoutine)
+            }
+        }
+    }
+
+    private fun onSuccessGetDailyRoutine(data: DailyRoutineListThemeTotalModel) {
+        updateState(
+            uiState.value.copy(
+                routineTotalList = data.themeTotalList
+            )
+        )
     }
 }
