f25819bd7611b072b78c16c5ad14dcc91dc8e009
emjayMJkim <minjeong030420@gmail.com>
2025-02-20 04:04:06 +0900

    #5 [ui] top 테마 chip

--- feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoiceScreen.kt
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoiceScreen.kt
@@ -1,20 +1,40 @@
 package com.sopetit.onboarding.routinechoice
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.sopetit.design_system.Gray0
+import com.sopetit.design_system.Gray200
 import com.sopetit.design_system.Gray50
+import com.sopetit.design_system.Gray700
+import com.sopetit.design_system.R
 import com.sopetit.design_system.RoutineChoiceBtn
 import com.sopetit.design_system.RoutineChoiceTopOriginalSpeech
+import com.sopetit.design_system.SoftieTypo
 import com.sopetit.domain.entity.enums.DollType
 import com.sopetit.domain.entity.request.CreateMemberModel
 import com.sopetit.ui.common.button.BottomRectangleBtn
@@ -73,10 +93,65 @@ fun RoutineChoiceContent(
                     dollType = selectedDollType.value,
                     speechContent = RoutineChoiceTopOriginalSpeech
                 )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                RoutineChoiceTopTheme()
             }
 
             BottomRectangleBtn(
                 btnTextContent = RoutineChoiceBtn,
+            )
+        }
+    }
+}
+
+@Composable
+fun RoutineChoiceTopTheme() {
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(Gray200)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .padding(vertical = 4.dp, horizontal = 4.dp)
+        ) {
+            items(3, key = { it }) { index ->
+                RoutineChoiceTopThemeItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun RoutineChoiceTopThemeItem() {
+    Box(
+        modifier = Modifier
+            .width(106.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .background(Gray0)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .align(Alignment.Center)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_theme1),
+                contentDescription = "theme icon",
+                modifier = Modifier
+                    .size(16.dp)
+            )
+
+            Text(
+                text = "한 걸음 성장",
+                color = Gray700,
+                style = SoftieTypo.body2,
+                modifier = Modifier
+                    .padding(start = 2.dp)
             )
         }
     }
