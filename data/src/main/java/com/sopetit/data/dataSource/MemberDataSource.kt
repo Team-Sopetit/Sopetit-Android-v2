7e6088d27f319ee3035c793dfd7026b55eb09347
emjayMJkim <minjeong030420@gmail.com>
2025-02-23 02:17:33 +0900

    #5 [add] 홈 화면 정보 서버통신 data layer 생성

--- data/src/main/java/com/sopetit/data/dataSource/MemberDataSource.kt
+++ data/src/main/java/com/sopetit/data/dataSource/MemberDataSource.kt
@@ -2,8 +2,10 @@ package com.sopetit.data.dataSource
 
 import com.sopetit.data.base.BaseResponse
 import com.sopetit.data.entity.request.member.CreateMemberRequestDto
+import com.sopetit.data.entity.response.member.GetMemberResponseDto
 import retrofit2.Response
 
 interface MemberDataSource {
     suspend fun postCreateMember(request: CreateMemberRequestDto): Response<BaseResponse<Unit>>
+    suspend fun getMember(): Response<BaseResponse<GetMemberResponseDto>>
 }
