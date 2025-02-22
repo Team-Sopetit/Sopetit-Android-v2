91f0eec87601e7ab2fc2b0f775efb7501cee3430
emjayMJkim <minjeong030420@gmail.com>
2025-02-23 02:19:53 +0900

    #5 [add] 홈 화면 정보 서버통신 domain layer 생성

--- domain/src/main/java/com/sopetit/domain/repository/MemberRepository.kt
+++ domain/src/main/java/com/sopetit/domain/repository/MemberRepository.kt
@@ -1,8 +1,10 @@
 package com.sopetit.domain.repository
 
 import com.sopetit.domain.entity.request.CreateMemberModel
+import com.sopetit.domain.entity.response.member.GetMemberModel
 import kotlinx.coroutines.flow.Flow
 
 interface MemberRepository {
     suspend fun postCreateMember(request: CreateMemberModel): Flow<Result<Unit>>
+    suspend fun getMember(): Flow<Result<GetMemberModel>>
 }
