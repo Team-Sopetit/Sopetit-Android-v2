cc6eded115164345be41374325aac335e9399376
emjayMJkim <minjeong030420@gmail.com>
2025-02-24 19:52:33 +0900

    #9 [feat] 홈 화면 background frame img 적용

--- feature/home/src/main/java/com/sopetit/home/HomeScreen.kt
+++ feature/home/src/main/java/com/sopetit/home/HomeScreen.kt
@@ -1,32 +1,41 @@
 package com.sopetit.home
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.sopetit.design_system.Gray50
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun HomeScreen() {
     val viewModel: HomeViewModel = hiltViewModel()
     val uiState: HomePageState by viewModel.uiState.collectAsStateWithLifecycle()
-    
-    HomeScreenContent()
+
+    HomeScreenContent(
+        backGroundImg = uiState.homeMemberModel.frameImageUrl
+    )
 }
 
 @Composable
-fun HomeScreenContent() {
+fun HomeScreenContent(
+    backGroundImg: String = "",
+) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Gray50)
     ) {
-        //
+        Image(
+            painter = rememberAsyncImagePainter(model = backGroundImg),
+            contentDescription = "background frame",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
