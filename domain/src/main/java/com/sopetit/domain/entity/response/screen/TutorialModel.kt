57d01bb37aef1d0516f462f725569e35083fd5ab
emjayMJkim <minjeong030420@gmail.com>
2025-02-27 03:50:50 +0900

    #9 [add] tutorial model 생성

---
+++ domain/src/main/java/com/sopetit/domain/entity/response/screen/TutorialModel.kt
@@ -0,0 +1,8 @@
+package com.sopetit.domain.entity.response.screen
+
+data class TutorialModel(
+    val bottomSheetTitle: String,
+    val title: String,
+    val semiTitle: String,
+    val tutorialImg: Int,
+)
--- feature/home/src/main/java/com/sopetit/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/sopetit/home/HomeViewModel.kt
@@ -1,7 +1,9 @@
 package com.sopetit.home
 
 import androidx.lifecycle.viewModelScope
+import com.sopetit.design_system.R
 import com.sopetit.domain.entity.response.member.GetMemberModel
+import com.sopetit.domain.entity.response.screen.TutorialModel
 import com.sopetit.domain.usecase.member.GetMemberUseCase
 import com.sopetit.ui.base.BaseViewModel
 import com.sopetit.ui.common.type.BearType
@@ -18,7 +20,37 @@ class HomeViewModel @Inject constructor(
 ) {
 
     init {
+        initSetTutorial()
         initGetHomeMember()
+    }
+
+    private fun initSetTutorial() {
+        val tutorials: List<TutorialModel> = listOf(
+            TutorialModel(
+                "데일리 루틴",
+                "매일 작은 성취감을 만드는 루틴",
+                "매일 실천할 수 있는 쉬운 루틴을 제안해요.\n루틴을 완료하면 솜뭉치를 얻어요.",
+                R.drawable.ic_tutorial_page1
+            ),
+            TutorialModel(
+                "챌린지",
+                "나를 찾아가는 특별한 루틴",
+                "특별한 일상을 위한 챌린지형 루틴이에요.\n루틴을 완료하면 무지개 솜뭉치를 얻어요.",
+                R.drawable.ic_tutorial_page2
+            ),
+            TutorialModel(
+                "솜뭉치 주기",
+                "솜뭉치를 얻으면?",
+                "얻은 솜뭉치는 곰인형에게 줄 수 있어요!\n곰인형이 어떤 반응을 보일까요?",
+                R.drawable.ic_tutorial_page3
+            )
+        )
+
+        updateState(
+            uiState.value.copy(
+                tutorialList = tutorials
+            )
+        )
     }
 
     private fun initGetHomeMember() {
