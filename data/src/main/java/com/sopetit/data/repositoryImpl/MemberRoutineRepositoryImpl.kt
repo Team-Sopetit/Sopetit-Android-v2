b9a4c02efeaf3c65023ac0b407ab26a1b96cf3e6
emjayMJkim <minjeong030420@gmail.com>
2025-03-17 17:48:18 +0900

    #12 [add] 데일리 루틴 삭제 domain layer 생성

--- data/src/main/java/com/sopetit/data/repositoryImpl/MemberRoutineRepositoryImpl.kt
+++ data/src/main/java/com/sopetit/data/repositoryImpl/MemberRoutineRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.sopetit.data.repositoryImpl
 
 import com.sopetit.data.dataSource.MemberRoutineDataSource
+import com.sopetit.data.mapper.memberroutine.DeleteMemberDailyRoutineMapper
+import com.sopetit.data.mapper.memberroutine.DeleteMemberDailyRoutineMapper.toDto
 import com.sopetit.data.mapper.memberroutine.GetMemberDailyRoutineMapper
+import com.sopetit.domain.entity.request.routine.DeleteDailyRoutineRequestModel
 import com.sopetit.domain.entity.response.memberroutine.MemberDailyRoutineTotalModel
 import com.sopetit.domain.repository.MemberRoutineRepository
 import kotlinx.coroutines.flow.Flow
@@ -12,4 +15,12 @@ class MemberRoutineRepositoryImpl @Inject constructor(
 ) : MemberRoutineRepository {
     override suspend fun getMemberDailyRoutine(): Flow<Result<MemberDailyRoutineTotalModel>> =
         GetMemberDailyRoutineMapper.responseToModel(apiCall = { memberRoutineDataSource.getMemberRoutine() })
+
+    override suspend fun deleteMemberDailyRoutine(request: DeleteDailyRoutineRequestModel): Flow<Result<Unit>> =
+        DeleteMemberDailyRoutineMapper.responseToModel(apiCall = {
+            memberRoutineDataSource.deleteMemberRoutine(
+                request.toDto()
+            )
+        })
+
 }
--- domain/src/main/java/com/sopetit/domain/repository/MemberRoutineRepository.kt
+++ domain/src/main/java/com/sopetit/domain/repository/MemberRoutineRepository.kt
@@ -1,8 +1,11 @@
 package com.sopetit.domain.repository
 
+import com.sopetit.domain.entity.request.routine.DeleteDailyRoutineRequestModel
 import com.sopetit.domain.entity.response.memberroutine.MemberDailyRoutineTotalModel
 import kotlinx.coroutines.flow.Flow
 
 interface MemberRoutineRepository {
     suspend fun getMemberDailyRoutine(): Flow<Result<MemberDailyRoutineTotalModel>>
+
+    suspend fun deleteMemberDailyRoutine(request: DeleteDailyRoutineRequestModel): Flow<Result<Unit>>
 }
