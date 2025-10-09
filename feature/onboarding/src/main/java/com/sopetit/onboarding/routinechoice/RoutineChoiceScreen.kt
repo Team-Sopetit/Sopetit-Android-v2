531cf27d28d86c1efa9aa87d33b635eaab714ee0
emjayMJkim <minjeong030420@gmail.com>
2025-10-09 21:06:25 +0900

    #21 [mod] routineChoiceScreen themeItem UI 수정

--- feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoiceScreen.kt
+++ feature/onboarding/src/main/java/com/sopetit/onboarding/routinechoice/RoutineChoiceScreen.kt
@@ -9,13 +9,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -34,6 +32,7 @@ import com.sopetit.design_system.Gray0
 import com.sopetit.design_system.Gray200
 import com.sopetit.design_system.Gray50
 import com.sopetit.design_system.Gray700
+import com.sopetit.design_system.R
 import com.sopetit.design_system.Red200
 import com.sopetit.design_system.RoutineChoiceBtn
 import com.sopetit.design_system.RoutineChoiceSnackBar
@@ -44,7 +43,6 @@ import com.sopetit.design_system.SoftieTypo
 import com.sopetit.domain.entity.enums.DollType
 import com.sopetit.domain.entity.request.CreateMemberModel
 import com.sopetit.domain.entity.response.routine.DailyRoutineListItemModel
-import com.sopetit.design_system.R
 import com.sopetit.onboarding.model.SelectedThemeItem
 import com.sopetit.ui.common.button.BottomRectangleBtn
 import com.sopetit.ui.common.content.TopBearFaceSpeech
@@ -58,7 +56,7 @@ fun RoutineChoiceScreen(
     goBackToThemeChoicePage: () -> Unit = {},
     memberModel: SharedFlow<CreateMemberModel> = MutableSharedFlow(),
     showSnackBar: (String, Int, Int) -> Unit,
-    goToHomePage: () -> Unit = {}
+    goToHomePage: () -> Unit = {},
 ) {
     val viewModel: RoutineChoiceViewModel = hiltViewModel()
     val uiState: RoutineChoicePageState by viewModel.uiState.collectAsStateWithLifecycle()
@@ -75,6 +73,7 @@ fun RoutineChoiceScreen(
                 is RoutineChoiceEvent.OnSuccessCreateMember -> {
                     goToHomePage()
                 }
+
                 is RoutineChoiceEvent.IsOverRoutineNumSize -> {
                     showSnackBar(RoutineChoiceSnackBar, 104, R.drawable.ic_snackbar_caution)
                 }
@@ -109,7 +108,7 @@ fun RoutineChoiceContent(
     eachThemeRoutineList: List<DailyRoutineListItemModel> = emptyList(),
     onSelectRoutine: (Int) -> Unit = {},
     selectedRoutineIdList: List<Int> = emptyList(),
-    onClickBtnAction: () -> Unit = {}
+    onClickBtnAction: () -> Unit = {},
 ) {
 
     Box(
@@ -172,27 +171,25 @@ fun RoutineChoiceTopTheme(
     chipThemeList: List<SelectedThemeItem> = emptyList(),
     selectedRoutineNumForTheme: List<Int> = listOf(0, 0, 0),
     onSelectThemeId: (Int) -> Unit = {},
-    selectedThemeId: Int = -1
+    selectedThemeId: Int = -1,
 ) {
     Row(
         modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxWidth()
             .clip(RoundedCornerShape(8.dp))
             .background(Gray200)
-            .wrapContentSize(Alignment.Center)
+            .padding(vertical = 4.dp, horizontal = 4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
-        LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            modifier = Modifier
-                .padding(vertical = 4.dp, horizontal = 4.dp)
-        ) {
-            itemsIndexed(chipThemeList, key = { _, item -> item.themeId }) { index, item ->
-                RoutineChoiceTopThemeItem(
-                    title = if (selectedRoutineNumForTheme[index] == 0) item.title else "${item.title} ${selectedRoutineNumForTheme[index]}",
-                    themeIcon = item.themeIcon,
-                    onClick = { onSelectThemeId(item.themeId) },
-                    isSelectedTheme = selectedThemeId == item.themeId
-                )
-            }
+        chipThemeList.forEachIndexed { index, item ->
+            RoutineChoiceTopThemeItem(
+                title = if (selectedRoutineNumForTheme[index] == 0) item.title else "${item.title} ${selectedRoutineNumForTheme[index]}",
+                themeIcon = item.themeIcon,
+                onClick = { onSelectThemeId(item.themeId) },
+                isSelectedTheme = selectedThemeId == item.themeId,
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
@@ -202,19 +199,19 @@ fun RoutineChoiceTopThemeItem(
     title: String = "",
     themeIcon: Int = -1,
     onClick: () -> Unit = {},
-    isSelectedTheme: Boolean = false
+    isSelectedTheme: Boolean = false,
+    modifier: Modifier,
 ) {
-    Box(
-        modifier = Modifier
-            .width(106.dp)
+    Row(
+        modifier = modifier
             .clip(RoundedCornerShape(5.dp))
             .background(if (isSelectedTheme) Gray0 else Gray200)
-            .clickable { onClick() }
+            .clickable { onClick() },
+        horizontalArrangement = Arrangement.Center
     ) {
         Row(
             modifier = Modifier
                 .padding(vertical = 8.dp)
-                .align(Alignment.Center)
         ) {
             Image(
                 painter = painterResource(id = themeIcon),
@@ -238,7 +235,7 @@ fun RoutineChoiceTopThemeItem(
 fun RoutineChoiceForThemeContent(
     routineList: List<DailyRoutineListItemModel> = emptyList(),
     onSelectRoutine: (Int) -> Unit = {},
-    selectedRoutineIdList: List<Int> = emptyList()
+    selectedRoutineIdList: List<Int> = emptyList(),
 ) {
     LazyColumn(
         modifier = Modifier
